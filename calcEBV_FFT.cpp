6935a4c8bc4a4a2a90cb67bad22bfaf69bec14ad
John Doe <johndoe@example.com>
2024-04-04 15:04:00 +0800

    a0_f in calculating new time steps

--- calcEBV_FFT.cpp
+++ calcEBV_FFT.cpp
@@ -447,8 +447,8 @@ int calcEBV(fields *fi, par *par)
     float Tcyclotron = 2.0 * pi * mp[0] / (e_charge_mass * (par->Bmax + 1e-5f));
     float acc_e = par->Emax * e_charge_mass;
     float vel_e = sqrt(kb * Temp_e / e_mass);
-    float TE = (sqrt(vel_e * vel_e / (acc_e * acc_e) + 2 * a0 / acc_e) - vel_e / acc_e) * 1000;
-    float TE1 = a0 / par->Emax * (par->Bmax + .00001);
+    float TE = (sqrt(vel_e * vel_e / (acc_e * acc_e) + 2 * a0 * par->a0_f / acc_e) - vel_e / acc_e) * 10000;
+    float TE1 = a0 * par->a0_f / par->Emax * (par->Bmax + .00001);
     TE = max(TE, TE1);
     // cout << "Tcyclotron=" << Tcyclotron << ",Bmax= " << par->Bmax << ", TE=" << TE << ",Emax= " << par->Emax << endl;
     if (TE < (par->dt[0] * 2 * f1 * ncalc0[0])) // if ideal time step is lower than actual timestep
