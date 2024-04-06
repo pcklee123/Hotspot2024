583a2eded7f44f1ed398a41533e9b842a8c0fd45
Home <pcklee123@gmail.com>
2024-04-06 15:08:24 +0800

    a

--- TS3.cpp
+++ TS3.cpp
@@ -74,7 +74,7 @@ int main()
     // float TEs = a0 * par->a0_f * vel_e;
     TE = TE <= 0 ? a0 * par->a0_f * vel_e : TE; // if acc is negligible
     // set time step to allow electrons to gyrate if there is B field or to allow electrons to move slowly throughout the plasma distance
-    par->dt[0] = min(Tcyclotron / 4, TE/2 ) / f1; // electron should not move more than 1 cell after ncalc*dt and should not make more than 1/4 gyration and must calculate E before the next 1/4 plasma period
+    par->dt[0] = min(Tcyclotron / 4, TE/8 ) / f1; // electron should not move more than 1 cell after ncalc*dt and should not make more than 1/4 gyration and must calculate E before the next 1/4 plasma period
     par->dt[1] = par->dt[0] * md_me;
 #define generateRandom
 #ifdef generateRandom
