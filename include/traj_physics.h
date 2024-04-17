2b770835d5202f3bcfd962295c03de087ab9aedb
John Doe <johndoe@example.com>
2024-04-17 11:31:32 +0800

    a

--- include/traj_physics.h
+++ include/traj_physics.h
@@ -9,7 +9,7 @@
 constexpr double weibullb = 4; // b factor for weibull. larger means closer to a shell. ~1 means filled more at the center.
 #define Temp_e 1e7             // in Kelvin 1e7 ~1keV
 #define Temp_d 1e7             // in Kelvin
-constexpr int f1 = 1;          // make bigger to make smaller time steps // 8 is min for sphere slight increas in KE
+constexpr int f1 = 4;          // make bigger to make smaller time steps // 8 is min for sphere slight increas in KE
 constexpr int f2 = f1 * 1.2;
 constexpr float incf = 1.2f;        // increment
 constexpr float decf = 1.0f / incf; // decrement factor
@@ -18,7 +18,7 @@ constexpr int n_space = 32; // should be 2 to power of n for faster FFT 128^3 = 
 
 constexpr size_t n_partd = 1 * 1024 * 1024; // n_space * n_space * n_space * 1 * 16; // must be 2 to power of n
 constexpr size_t n_parte = n_partd;
-constexpr size_t nback = n_partd / 4; // background stationary particles distributed over all cells - improves stability
+constexpr size_t nback = n_partd / 32; // background stationary particles distributed over all cells - improves stability
 
 constexpr float R_s = n_space / 1;                                 // LPF smoothing radius
 constexpr float r0_f[3] = {n_space / 8 + 1, n_space / 8, n_space}; //  radius of sphere or cylinder (electron, ion, plasma)
