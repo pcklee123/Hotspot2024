f1020a88393f9f7749a99b91e411346c32e93484
John Doe <johndoe@example.com>
2024-04-19 09:35:58 +0800

    a

--- include/traj_physics.h
+++ include/traj_physics.h
@@ -57,7 +57,7 @@ constexpr int md_me = 60;        // ratio of electron speed/deuteron speed at th
 #define trilinon_
 
 #define Eon_     // whether to calculate the internally generated electric (E) field externally applied fields are always on
-#define Uon_     // whether to calculate the electric (V) potential and potential energy (U). Needs Eon to be enabled.
+//#define Uon_     // whether to calculate the electric (V) potential and potential energy (U). Needs Eon to be enabled.
 #define UE_field //
 #define Bon_     // whether to calculate the internally generated magnetic (B) field
 #define UB_field
