2905cfe98d3bb3c152b6c19ee42b4a4f7f2d16a6
Home <pcklee123@gmail.com>
2024-04-30 06:28:03 +0800

    a

--- cl_code.cpp
+++ cl_code.cpp
@@ -127,7 +127,12 @@ void cl_start(fields *fi, particles *pt, par *par)
 
     cl::Program program(context, sources);
 
-    //cl_build_options << " -cl-mad-enable -cl-fast-relaxed-math -cl-no-signed-zeros -cl-denorms-are-zero -cl-single-precision-constant"; // Add other options as needed -cl-unsafe-math-optimizations -cl-finite-math-only -cl-no-signed-zeros -cl-denorms-are-zero -cl-single-precision-constant
+    std::string deviceVendor = default_device.getInfo<CL_DEVICE_VENDOR>();
+    if(deviceVendor.find("Intel") != std::string::npos) {
+        cl_build_options << "-cl-mad-enable -cl-fast-relaxed-math -cl-no-signed-zeros -cl-denorms-are-zero -cl-single-precision-constant";
+    } else if(deviceVendor.find("Advanced Micro Devices") != std::string::npos) {
+        cl_build_options << "-O3 -cl-mad-enable -cl-fast-relaxed-math -cl-no-signed-zeros -cl-denorms-are-zero -cl-single-precision-constant";
+    }
     cl_int cl_err = program.build({default_device}, cl_build_options.str().c_str());
     info_file << "building: " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device);
     if (cl_err != CL_SUCCESS)
