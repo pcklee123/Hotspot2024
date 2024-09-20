4b9929212212bd6af48578ed7cbd549a05f0d7b9
pcklee123 <99310789+pcklee123@users.noreply.github.com>
2024-09-20 10:04:41 +0800

    a

--- calcEBV_vkFFT.cpp
+++ calcEBV_vkFFT.cpp
@@ -575,12 +575,13 @@ int calcEBV(fields *fi, par *par)
                                             // resFFT = transferDataToCPU(&vkGPU, &fft_real[0][0], &fi->fft_real_buffer, bufferSize_R3);
 
 #ifdef dB_dton_
-        clEnqueueCopyBuffer(vkGPU.commandQueue, fi->B_buffer, fi->B0_buffer, 0, 0, n_cellsf * 3, 0, NULL, NULL); // store B(t) in B0 will replace thist with Bdot later
+        clEnqueueCopyBuffer(vkGPU.commandQueue, fi->B_buffer, fi->B0_buffer, 0, 0, n_cellsf * 3, 0, NULL, NULL); // store B(t) in B0 will replace this with Bdot later
         res = clFinish(vkGPU.commandQueue);
 #endif
         clSetKernelArg(sumFftFieldB_kernel, 0, sizeof(cl_mem), &fi->fft_real_buffer); // real[0-2] is E field
         clSetKernelArg(sumFftFieldB_kernel, 1, sizeof(cl_mem), &fi->Be_buffer);
         clSetKernelArg(sumFftFieldB_kernel, 2, sizeof(cl_mem), &fi->B_buffer);
+
         res = clEnqueueNDRangeKernel(vkGPU.commandQueue, sumFftFieldB_kernel, 1, NULL, &n_cells, NULL, 0, NULL, NULL); //  Enqueue NDRange kernel
         if (res)
             cout << "sumFftFieldB_kernel B  res: " << res << endl;
